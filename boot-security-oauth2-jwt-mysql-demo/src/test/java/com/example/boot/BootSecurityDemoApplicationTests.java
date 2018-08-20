1cd945456bb8fb5bdc67878a5585a9f3c90785dc
ompal singh <ompal431@gmail.com>
2018-08-21 01:12:06 +0530

    testcase

--- boot-security-oauth2-jwt-mysql-demo/src/test/java/com/example/boot/BootSecurityDemoApplicationTests.java
+++ boot-security-oauth2-jwt-mysql-demo/src/test/java/com/example/boot/BootSecurityDemoApplicationTests.java
@@ -1,20 +1,13 @@
 package com.example.boot;
 
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
 
-import com.example.boot.controller.RoleController;
-import com.example.boot.entity.Role;
-
-@RunWith(SpringRunner.class)
-@WebMvcTest(value = RoleController.class, secure = false)
+@RunWith(SpringRunner.class) 
 public class BootSecurityDemoApplicationTests {
 
-	@Autowired
-	private MockMvc mockMvc;
+	/*@Autowired
+	private MockMvc mockMvc;*/
 	 
 	//Role mocRole = new Role(1,"HR");
 	
